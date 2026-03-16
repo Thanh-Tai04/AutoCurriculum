312b13a44c1ef4ef8ec61a72a5f493690ce9f9cc
ThanhTai <tainguyen280404@gmail.com>
2026-03-16 17:22:16 +0700

    v3

--- obj/Debug/net8.0/AutoCurriculum.AssemblyInfo.cs
+++ obj/Debug/net8.0/AutoCurriculum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoCurriculum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58ab6c089e247e378dd7b515e78aed334cc6039")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb6bfc9f50729bfe5e31d39ddc932e04ec54a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoCurriculum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoCurriculum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
